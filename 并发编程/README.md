5bb22a2b9a4335394c6a4cd82bfc3353c7dc129f
liyohe <liyohe.cn@gmail.com>
2022-09-15 21:36:45 +0800

    [ADD] 并发编程

---
+++ 并发编程/README.md
@@ -0,0 +1,102 @@
+# Java-并发编程
+
+## 概览
+
+## 进程与线程
+
+## Java线程
+
+### 本章内容
+
+### 创建/运行线程
+
+### 观察多个线程同时运行
+
+### 查看/进程线程的方法
+
+### 原理之线程运行
+
+### 常见方法
+
+### start 与 run
+
+### sleep 与 yield
+
+### join 方法详解
+
+### interrupt 方法详解
+
+### 不推荐的方法
+
+### 主线程与守护线程
+
+### 五种状态
+
+### 六种状态
+
+### 本章小结
+
+[详细内容](./docs/Java线程.md)
+
+## 共享模型-管程
+
+## 共享模型-内存
+
+## 共享模型--无锁
+
+## 共享模型-不可变
+
+### 日期转换的问题
+
+### 不可变设计
+
+### 无状态
+
+## 共享模型-工具
+
+### 线程池
+
+#### 自定义线程池
+
+#### ThreadPoolExecutor
+
+#### Fork/Join
+
+[详细内容](./docs/线程池.md)
+
+### J.U.C
+
+#### AQS原理
+
+#### ReentrantLock 原理
+
+#### 读写锁
+
+#### Semaphore
+
+#### CountdownLatch
+
+#### CyclicBarrier
+
+#### 线程安全集合类概述
+
+#### ConcurrentHashMap
+
+#### BlockingQueue
+
+#### ConcurrentLinkedQueue
+
+#### CopyOnWriteArrayList
+
+[详细内容](./docs/J.U.C.md)
+
+
+
+
+
+
+
+
+
+
+
---
+++ 并发编程/code/src/main/java/cn/liyohe/juc/chapter8/threadpool/ThreadPoolStatus.java
@@ -0,0 +1,36 @@
+package cn.liyohe.juc.chapter8.threadpool;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.concurrent.*;
+
+/**
+ * @author Liyohe
+ */
+
+@Slf4j(topic = "ThreadPoolStatus-")
+public class ThreadPoolStatus {
+
+    private static final ThreadPoolExecutor poolExecutor = new ThreadPoolExecutor(
+            2, 4, 60, TimeUnit.SECONDS, new LinkedBlockingDeque<>(10));
+
+    public static void main(String[] args) {
+        status();
+        ExecutorService executorService = Executors.newFixedThreadPool(10);
+        ExecutorService executorService1 = Executors.newCachedThreadPool();
+
+    }
+
+    public static void status() {
+        boolean terminated = poolExecutor.isTerminated();
+        log.info("terminated={}", terminated);
+        poolExecutor.shutdown();
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        log.info("terminated={}", terminated);
+    }
+
+}
